894e1b696a195625c6b0764e73e45f9a9e26e785
Pedro Arenas <pedroars99@icloud.com>
2022-10-27 19:13:50 +0200

    Change app name

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_service_plan" "optring_sp" {
 }
 
 resource "azurerm_linux_web_app" "optring_app" {
-  name                = "example"
+  name                = "${var.name}-app"
   resource_group_name = azurerm_resource_group.optring_rg.name
   location            = azurerm_resource_group.optring_rg.location
   service_plan_id     = azurerm_service_plan.optring_sp.id
