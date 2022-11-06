4be0c9dea587b20b44c8ca58457e677bbfd87594
Pedro Arenas <pedroars99@icloud.com>
2022-11-06 17:17:59 +0100

    Set Up Mongo Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true}),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
   ],
   controllers: [AppController],
   providers: [AppService],
