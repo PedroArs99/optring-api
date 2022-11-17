d6ecc6a9072be86e2f828cd245390cb513266001
Pedro Arenas <pedroars99@icloud.com>
2022-11-17 23:25:14 +0100

    Configure Graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './modules/test/test.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+      include: [TestModule],
+      sortSchema: true,
+    }),
     MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
-    TestModule
+    TestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
