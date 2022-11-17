22ea37669b3d9a9d7869ba74b703c5d3d81a9320
Pedro Arenas <pedroars99@icloud.com>
2022-11-17 23:25:47 +0100

    Define CatsResolver

---
+++ src/modules/test/graphql/cat.model.ts
@@ -0,0 +1,16 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType({ description: 'cat' })
+export class Cat {
+  @Field(type => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  age: number;
+
+  @Field()
+  breed: string;
+}
--- src/modules/test/test.module.ts
+++ src/modules/test/test.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CatsController } from './Cat.controller';
 import { Cat, CatSchema } from './Cat.entity';
 import { CatService } from './Cat.service';
+import { CatsResolver } from './graphql/cats.resolver';
 
 @Module({
   controllers: [CatsController],
   imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
-  providers: [CatService],
+  providers: [
+    CatsResolver,
+    CatService
+  ],
 })
 export class TestModule {}
