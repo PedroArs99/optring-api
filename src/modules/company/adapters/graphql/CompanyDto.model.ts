3d7020f019eb278b955ee639862bd5c42f74b478
Pedro Arenas <pedroars99@icloud.com>
2022-12-22 10:38:30 +0100

    Add Areas to the  company graphql schema

--- src/modules/company/adapters/graphql/CompanyDto.model.ts
+++ src/modules/company/adapters/graphql/CompanyDto.model.ts
@@ -1,5 +1,7 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { GraphQLList } from 'graphql';
 import { Company } from '../../models/Company.model';
+import { AreaDto } from './AreaDto.model';
 
 @ObjectType({ description: 'company' })
 export class CompanyDto {
@@ -12,11 +14,15 @@ export class CompanyDto {
   @Field()
   contactEmail: string;
 
+  @Field(type => [AreaDto])
+  areas: AreaDto[];
+
   static fromDomain(company: Company): CompanyDto {
     return {
       id: company.id,
       name: company.name,
-      contactEmail: company.contactEmail
+      contactEmail: company.contactEmail,
+      areas: company.areas.map(area => AreaDto.fromDomain(area))
     }
   }
 }
