8bd308447b9bd7069b6d54095a9905dd97d9783d
Pedro Arenas <pedroars99@icloud.com>
2023-01-05 22:51:25 +0100

    Create RegisterUserCommand

--- src/modules/auth/RegisterUser.command.ts
+++
@@ -1,5 +0,0 @@
-export interface RegisterUserCommand {
-    email: string;
-    name: string;
-    password: string;
-}
---
+++ src/modules/auth/application/commands/RegisterUser.command.ts
@@ -0,0 +1,9 @@
+export class RegisterUserCommand {
+    constructor(
+        public readonly email: string,
+        public readonly name: string,
+        public readonly password: string,
+    ){}
+}
+
+
